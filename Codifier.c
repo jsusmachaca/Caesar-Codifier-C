33656051c65bbd87c6f3c3fca64a19cae539620a
Jesus Machaca <falcorgd@gmail.com>
2023-08-07 11:49:59 -0500

    Final Version

--- Codifier.c
+++ Codifier.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void decoder(char word[]) {
-     char abc[] = "nopqrstuvwxyzabcdefghijklm"; 
-     char denc[] = "abcdefghijklmnopqrstuvwxyz"; // cva cba
-     char word_denc[strlen(word)];
-
-     for (int f = 0; f < strlen(word); f ++ ){
-          if (word[f] == ' ' || word[f] == '\n'){
-               word_denc[f] = ' ';
-          }
-          else {
-               char lett = word[f];
-               for(int i= 0; i < strlen(abc); i++){
-                    if(lett == abc[i]){
-                         word_denc[f] = denc[i];
-                         break;
-                    }
-               }
-          }
-     }    
-     word_denc[strlen(word)] = '\0';
-     printf("Frase decodificada => %s\n", word_denc);
-}
 
 char encoder(char word[]) {
      char abc[] = "abcdefghijklmnopqrstuvwxyz"; // cva cba
@@ -53,23 +31,24 @@ char encoder(char word[]) {
      printf("Frase codificada => %s\n", word_enc);
 }
 
-int main() {
-     char frase[300];
-     char req;
-     printf("¿Qué deseas hacer?\nCodificar [e] Decodificar [d]\n");
-     scanf("%c", &req);
-     getchar();
-     if (req == 'e') {
-          printf("Ingresa una frase: \n");
-          fgets(frase, sizeof(frase), stdin);
-          encoder(frase);
-     }
-     else if (req == 'd') {
-          printf("Ingresa una frase: \n");
-          fgets(frase, sizeof(frase), stdin);
-          decoder(frase);
-     }
-     else {
-          printf("Por favor ingrese la opción correcta\n");
+int main(int arg, char *args[]) {
+     char word[300];
+     for (int i = 1; i < arg; i++){
+          if (strcmp(args[1], "-c") == 0 || strcmp(args[1], "--console") == 0 && arg <= 2){
+               fgets(word, sizeof(word), stdin);
+               encoder(word);
+          }
+
+          else if(strcmp(args[1], "-c") != 0 && strcmp(args[1], "--help") != 0) {
+               encoder(args[i]);
+          }
+          
+          else if(strcmp(args[1], "--help") == 0) {
+               printf("ARGS:\nCodifier <WORD>, para codificar palabra por palabra .\n\nOPTIONS:\n-c, --console\nIngresar a modo consola para dictar palabras .\n\n");
+               
+          }else {
+               printf("Parametros incorrectos use --help para más información");
+               break;
+          }
      }
 }
